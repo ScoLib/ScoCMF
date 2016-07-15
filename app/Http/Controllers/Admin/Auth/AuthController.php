52dc4a114e0f8688741d4d50905d82d9d525902b
klgd <slice2008@qq.com>
2016-07-15 18:45:23 +0800

    整理资源文件

---
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace Sco\Http\Controllers\Admin\Auth;
+
+use Auth;
+use Carbon\Carbon;
+//use Illuminate\Auth\Authenticatable;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+//use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Sco\Http\Controllers\Controller;
+use Route;
+
+class AuthController extends Controller
+{
+    use ThrottlesLogins, AuthenticatesUsers;
+
+    //private $maxLoginAttempts = 2;
+
+    //private $lockoutTime = 10;
+
+    protected $loginView = 'admin::auth.login';
+
+    public function __construct()
+    {
+        $this->middleware('guest:admin', ['except' => 'logout']);
+    }
+
+    /*public function getLogin()
+    {
+        return $this->render('admin::auth.login');
+    }*/
+
+    public function postLogin1(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        // 登录失败次数限制
+        if ($lockedOut = $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if (Auth::guard('admin')->attempt($request->only(['email', 'password']), true)) {
+            $this->updateLogin();
+
+            return redirect()->route('admin.index');
+        }
+        return redirect()->back()
+            ->withInput($request->only($this->loginUsername(), 'remember'))
+            ->withErrors('您的用户名或密码错误');
+
+    }
+
+    public function getLogout1()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+
+    protected function loginUsername1()
+    {
+        return 'username';
+    }
+
+    private function updateLogin()
+    {
+        $user = Auth::guard('admin')->user();
+
+        $user->last_login_ip   = request()->ip();
+        $user->last_login_time = Carbon::now();
+        $user->save();
+    }
+
+
+}
---
+++ resources/admin/layouts/auth.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    @include('admin::layouts.partials.htmlheader')
+    <!-- iCheck -->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="../../index2.html"><b>Sco</b>CMF</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+        <p class="login-box-msg">
+            @if (count($errors) > 0)
+                {{ $errors->first() }}
+            @else
+                请输入您的用户名和密码
+            @endif
+        </p>
+
+        <form action="{{ route('admin.login') }}" method="post">
+            {!! csrf_field() !!}
+            <div class="form-group has-feedback">
+                <input type="email" name="email" class="form-control" placeholder="邮箱">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" name="password" class="form-control" placeholder="密码">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <div class="col-xs-8">
+                    <div class="checkbox">
+                        <label>
+                            <input type="checkbox"> 记住我
+                        </label>
+                    </div>
+                </div>
+                <!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button>
+                </div>
+                <!-- /.col -->
+            </div>
+        </form>
+
+    </div>
+    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+@include('admin::layouts.partials.script')
+
+</body>
+</html>
