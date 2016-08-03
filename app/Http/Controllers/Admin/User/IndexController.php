50cd3a088841265f5320874e0adba2709a437ef9
klgd <slice2008@qq.com>
2016-08-03 17:51:00 +0800

    refactor: add User/IndexController.php

---
+++ app/Http/Controllers/Admin/User/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sco\Http\Controllers\Admin\User;
+
+use Sco\Http\Controllers\Admin\BaseController;
+
+class IndexController extends BaseController
+{
+    public function getIndex()
+    {
+
+    }
+}
